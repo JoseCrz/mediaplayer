f2e8f1400f0354cc5ac430780c48f8d813c1e348
Jose Cuevas <josecueram@gmail.com>
2020-04-19 15:09:38 -0500

    converted index to TS

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,16 +1,15 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
-import AutoPause from './plugins/AutoPause.ts'
+import MediaPlayer from './MediaPlayer'
+import AutoPlay from './plugins/AutoPlay'
+import AutoPause from './plugins/AutoPause'
 
 const $video = document.querySelector('#video')
-const $buttonPlay = document.querySelector('#button-play')
-const $buttonMute = document.querySelector('#button-mute')
+const $buttonPlay: HTMLElement = document.querySelector('#button-play')
+const $buttonMute: HTMLElement = document.querySelector('#button-mute')
 
 const player = new MediaPlayer({element: $video, plugins: [new AutoPlay(), new AutoPause()]})
 
 $buttonPlay.onclick = () => player.playPause()
 $buttonMute.onclick = () => player.muteUnmute()
-
 // if ('serviceWorker' in navigator) {
 //   navigator.serviceWorker.register('/sw.js').catch(error => console.log(error))
 // }
