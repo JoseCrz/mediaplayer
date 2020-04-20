e89e57df4b1796c47204190f14ede8d14ab707d9
Jose Cuevas <josecueram@gmail.com>
2020-04-19 22:24:49 -0500

    created Ads plugin

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,12 +1,16 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import Ads from './plugins/Ads'
 
 const $video = document.querySelector('#video')
 const $buttonPlay: HTMLElement = document.querySelector('#button-play')
 const $buttonMute: HTMLElement = document.querySelector('#button-mute')
 
-const player = new MediaPlayer({element: $video, plugins: [new AutoPlay(), new AutoPause()]})
+const player = new MediaPlayer({
+  element: $video, 
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()]
+})
 
 $buttonPlay.onclick = () => player.playPause()
 $buttonMute.onclick = () => player.muteUnmute()
---
+++ assets/js/plugins/Ads/index.ts
@@ -0,0 +1,41 @@
+import MediaPlayer from '../../MediaPlayer'
+import Ads, { Ad } from './Ads'
+
+class AdsPlugin {
+  private player: MediaPlayer
+  private media: HTMLMediaElement
+  private ads: Ads
+  private currentAd: Ad
+
+  constructor () {
+    this.ads = Ads.getInstance()
+    this.handleTimeUpdate = this.handleTimeUpdate.bind(this)
+  }
+
+  run(player: MediaPlayer) {
+    this.player = player
+    this.media = this.player.media
+    this.media.addEventListener('timeupdate', this.handleTimeUpdate)
+  }
+
+  private handleTimeUpdate () {
+    const currentTime = Math.floor(this.media.currentTime)
+    
+    if (currentTime % 30 === 0) {
+      this.renderAd()
+    }
+  }
+
+  private renderAd () {
+    if (this.currentAd) {
+      return
+    }
+    
+    const ad = this.ads.getAd()
+    this.currentAd = ad
+    console.log(this.currentAd)
+  }
+
+}
+
+export default AdsPlugin
